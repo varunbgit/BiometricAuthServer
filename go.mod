a41016d2c2144316cd5251bb9290bb1c34ced49f
Your New Username <varun.bansal@razorpay.com>
2024-09-20 16:13:58 +0530

    register endpoint

---
+++ go.mod
@@ -0,0 +1,16 @@
+module awesomeProject1
+
+go 1.22
+
+require (
+	github.com/Knetic/govaluate v3.0.0+incompatible // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
+	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/expr-lang/expr v1.16.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/nikunjy/rules v1.5.0 // indirect
+	github.com/solo-finance/logic-expression-parser v0.0.0-20230904111245-86f03160ba7b // indirect
+	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
+)
---
+++ main/BiometricAuth.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type RelyingParty struct {
+	Name string
+	Id   string
+}
+
+type User struct {
+	Id          string
+	Name        string
+	DisplayName string
+}
+
+type PublicCredParam struct {
+	Alg  int
+	Type string
+}
+
+type AuthenticatorSelection struct {
+	AuthenticatorAttachment string
+}
+
+// CreateOptions represents a user in the system
+type CreateOptions struct {
+	Challenge              string
+	Rp                     RelyingParty
+	User                   User
+	PublicKeyParams        []PublicCredParam
+	AuthenticatorSelection AuthenticatorSelection
+	Timeout                int
+	Attestation            string
+}
+
+// RegisterHandler handles user registration
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	publicCredParam := PublicCredParam{
+		Alg:  -7,
+		Type: "public-key",
+	}
+
+	var publicCredParams []PublicCredParam
+	publicCredParams = append(publicCredParams, publicCredParam)
+
+	createOptions := CreateOptions{
+		Challenge: uuid.New().String(),
+		Rp: RelyingParty{
+			Name: "Rzp",
+			Id:   "Rzp.com",
+		},
+		User: User{
+			Id:          "user_rzp_6088",
+			Name:        "8955496900",
+			DisplayName: "8955496900",
+		},
+		PublicKeyParams:        publicCredParams,
+		AuthenticatorSelection: AuthenticatorSelection{AuthenticatorAttachment: "cross-platform"},
+		Timeout:                60000,
+		Attestation:            "direct",
+	}
+
+	jsonData, err := json.Marshal(createOptions)
+	if err != nil {
+		http.Error(w, "Failed to serialize createOptions", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonData)
+	w.WriteHeader(http.StatusCreated)
+}
+
+// SaveHandler handles the /save endpoint
+func SaveHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the request method is POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	type Response struct {
+		ClientDataJson    interface{}
+		AttestationObject interface{}
+	}
+	// Define a struct to hold the incoming data
+	type PublicKeyCredential struct {
+		ID       string
+		RawId    string
+		Type     string
+		response Response
+	}
+
+	// Parse the request body
+	var saveRequest PublicKeyCredential
+	err := json.NewDecoder(r.Body).Decode(&saveRequest)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("Received data: ", saveRequest)
+
+	fmt.Println("printing the save request: ", saveRequest)
+	// Respond with a success message
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"status": "success", "id": saveRequest.ID})
+}
+
+// VerificationHandler handles user verification
+func VerificationHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("User verified successfully"))
+}
+
+func main() {
+	http.HandleFunc("/register", RegisterHandler)
+	http.HandleFunc("/verification", VerificationHandler)
+	http.HandleFunc("/save", SaveHandler)
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
